48f88e2efe12ce10cc7d1e5e54a10b1a7618e99a
architjain808 <architjain808@gmail.com>
2021-03-20 11:37:15 +0530

    first routing n login and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+const approute : Routes = [
+{path:"login" , component: LoginComponent},
+{path:"register" , component: RegisterComponent}
 
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(approute)
   ],
   providers: [],
   bootstrap: [AppComponent]
