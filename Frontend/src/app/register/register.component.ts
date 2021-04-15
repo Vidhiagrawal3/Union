9899a0909c2592cb49827ee80e7f8af93306caf3
Vidhiagrawal3 <vidhiagrawal305@gmail.com>
2021-04-15 20:26:30 +0530

    register basic

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  name = new FormControl('', [Validators.required, Validators.maxLength(15)]);
 
   constructor() { }
 
