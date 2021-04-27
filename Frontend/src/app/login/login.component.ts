d75b0f832351b07daa0f321571d6430ffe9030d4
architjain808 <architjain808@gmail.com>
2021-04-27 11:41:51 +0530

    push

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {Router} from '@angular/router';
-import {AlumniService } from 'src/assets/services/alumni.service';
+import {AlumniService } from '../services/alumni.service';
 
 @Component({
   selector: 'app-login',
