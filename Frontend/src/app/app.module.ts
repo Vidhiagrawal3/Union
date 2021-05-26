85aa456ef819f7ba7e424b97468fbd06a20f05c3
architjain808 <architjain808@gmail.com>
2021-05-26 11:40:11 +0530

    blog component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AlumniHomeComponent } from './alumni-home/alumni-home.component';
 import { HeaderComponent } from './header/header.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { AuthInterceptor } from './services/auth-interceptor';
+import { BlogsComponent } from './blogs/blogs.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { AuthInterceptor } from './services/auth-interceptor';
     RegisterComponent,
     AlumniHomeComponent,
     HeaderComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    BlogsComponent
   ],
   imports: [
     FormsModule,ReactiveFormsModule,
