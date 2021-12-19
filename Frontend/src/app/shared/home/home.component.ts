a33ead998327b8fd0530768ae45a91b6c9775b75
Vidhiagrawal3 <vidhiagrawal305@gmail.com>
2021-12-20 00:32:31 +0530

    blogs on home quite sorted

--- Frontend/src/app/shared/home/home.component.ts
+++ Frontend/src/app/shared/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
+import { EventsService } from 'src/app/services/events.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _blogging:BlogService, private _event: EventsService) { }
+
+  allblogs: any =[] ;
+  allevents:any=[];
 
   ngOnInit(): void {
+    this.allblogscall();
+    this.fetchevents()
   }
+
+  allblogscall(){
+    this._blogging.FetchBlog()
+ .subscribe(
+   data=>{this.allblogs = data},
+   error=>{console.error(error)}
+     )}
+
+  fetchevents(){
+     this._event.Fetchevents()
+     .subscribe(
+       data=>{this.allevents = data},
+      error=>{console.error(error)}
+     )}
+
+
 
 }
